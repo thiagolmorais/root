e7245395d09c151a6f43e4e837fc2fccc6cfc1f4
Thiago Morais <tf_lima@terra.com.br>
2017-12-13 23:34:01 -0200

    Contato

--- novo/contato.php
+++ novo/contato.php
@@ -41,10 +41,18 @@
 <br/>
 <br/>
 <div class="container">
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3644.738240881763!2d-46.41452468501063!3d-24.005018784010424!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce1db2e586da8d%3A0x271ae3e10bdc671e!2sFATEC!5e0!3m2!1spt-BR!2sbr!4v1512074849562" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+            
+
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3644.738102057435!2d-46.414524685432625!3d-24.005023684464437!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce1db2e586da8d%3A0x271ae3e10bdc671e!2sFATEC!5e0!3m2!1sen!2sbr!4v1513214162858" width=100% height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+            
+
+            <p>Praça 19 de Janeiro, 144 - Boqueirão, Praia Grande</p>
+            <p><img src="img/phone.png"> (13) 0800-0000</p>
+            <p><img src="img/whats.png"> (13) 98888-0000</p>
+            <p><img src="img/mail.png"> contato@storyteller.com.br</p>
+
 </div>
-
-
 
     <!-- Le javascript
     ================================================== -->
