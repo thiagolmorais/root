c948c4c8be03887d4cd964377734b420d81d5eb4
Henrique Hornos <henrique.hornos@gmail.com>
2017-12-14 22:03:41 -0200

    add base de dados

---
+++ novo/escola.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE escola;
+USE escola;
+
+CREATE TABLE aluno
+(
+	cd_aluno INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	cd_cpf VARCHAR(11) NOT NULL UNIQUE,
+	cd_perfil INTEGER NOT NULL,
+	nm_aluno VARCHAR(50) NOT NULL,
+	dt_nascimento VARCHAR(10) NOT NULL,
+	ds_endereco VARCHAR(200) NOT NULL,
+	mn_genero VARCHAR(10) NOT NULL,
+	nm_email VARCHAR(30) NOT NULL UNIQUE,
+	nm_usuario VARCHAR(20) NOT NULL UNIQUE,
+	nm_senha VARCHAR(60) NOT NULL,
+	nm_curso VARCHAR(20) NOT NULL
+);
